d203dfd0931aa23b3efb8d61ababc792f30a3687
ganatan <dannyganatan@gmail.com>
2020-07-27 11:39:41 +0200

    Update environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,6 @@ export const environment = {
     name: 'angular-starter',
     angular: 'Angular 10.0.5',
     bootstrap: 'Bootstrap 4.5.0',
-    fontawesome: 'Font Awesome 5.13.1',
+    fontawesome: 'Font Awesome 5.14.0',
   }
 };
