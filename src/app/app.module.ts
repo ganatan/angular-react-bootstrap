72f8e4001267c982a314bc8908fe78e11aa8177f
ganatan <dannyganatan@gmail.com>
2021-05-15 11:26:43 +0200

    fix app.modules.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,16 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './modules/general/home/home.component';
 import { NotFoundComponent } from './modules/general/not-found/not-found.component';
 import { AppRoutingModule } from './app-routing.module';
-import { MailingModule } from './modules/general/contact/mailing/mailing.module';
-import { MailingComponent } from './modules/general/contact/mailing/mailing.component';
-import { MapModule } from './modules/general/contact/map/map.module';
-import { MapComponent } from './modules/general/contact/map/map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NotFoundComponent,
-    MailingComponent,
-    MapComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MailingModule,
-    MapModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
