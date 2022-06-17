233e83b43b578649461747cc5f8d75777079b26f
ganatan <dannyganatan@gmail.com>
2022-06-17 16:10:22 +0200

    Update user-event and version on react app

--- react/src/environments/environment.js
+++ react/src/environments/environment.js
@@ -2,7 +2,7 @@ export const environment = {
   application:
   {
     name: 'react-bootstrap',
-    version: 'React 18.1.0',
+    version: 'React 18.2.0',
     bootstrap: 'Bootstrap 5.1.3',
     fontawesome: 'Font Awesome 6.1.1',
   }
