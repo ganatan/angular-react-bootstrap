4df25ae486ffc72e082d7891ad023e83259a28d6
ganatan <dannyganatan@gmail.com>
2022-07-19 14:27:06 +0200

    Update Angular 14.0.6 & ESLint on apps

--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   application:
   {
     name: 'angular-starter',
-    version: 'Angular 14.0.5',
+    version: 'Angular 14.0.6',
     bootstrap: 'Bootstrap 5.1.3',
     fontawesome: 'Font Awesome 6.1.1',
   }
