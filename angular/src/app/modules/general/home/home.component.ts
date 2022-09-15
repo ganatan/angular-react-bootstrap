85cd3ad6e0d36556dc4aea2093b5b680c949e586
ganatan <dannyganatan@gmail.com>
2022-09-15 09:24:47 +0200

    Update Angular 14.2.2 & React dependencies

--- angular/src/app/modules/general/home/home.component.ts
+++ angular/src/app/modules/general/home/home.component.ts
@@ -22,7 +22,7 @@ export class HomeComponent implements OnInit {
     this.meta.updateTag(
       {
         name: 'description',
-        content: 'This application was developed with Angular version 14.1.0 and bootstrap 5.2.0' +
+        content: 'This application was developed with Angular version 14.2.2 and bootstrap 5.2.1' +
           ' It applies Routing, Lazy loading, Server side rendering and Progressive Web App (PWA)'
       });
 
--- react/src/environments/environment.js
+++ react/src/environments/environment.js
@@ -3,7 +3,7 @@ export const environment = {
   {
     name: 'react-bootstrap',
     version: 'React 18.2.0',
-    bootstrap: 'Bootstrap 5.2.0',
-    fontawesome: 'Font Awesome 6.1.2',
+    bootstrap: 'Bootstrap 5.2.1',
+    fontawesome: 'Font Awesome 6.2.0',
   }
 };
