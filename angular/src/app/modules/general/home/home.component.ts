866417ce7caac5036bfca456937480e30030eeba
ganatan <dannyganatan@gmail.com>
2022-09-29 11:53:39 +0200

    Update Angular 14.2.4 & ESLint on React

--- angular/src/app/modules/general/home/home.component.ts
+++ angular/src/app/modules/general/home/home.component.ts
@@ -22,7 +22,7 @@ export class HomeComponent implements OnInit {
     this.meta.updateTag(
       {
         name: 'description',
-        content: 'This application was developed with Angular version 14.2.2 and bootstrap 5.2.1' +
+        content: 'This application was developed with Angular version 14.2.4 and bootstrap 5.2.1' +
           ' It applies Routing, Lazy loading, Server side rendering and Progressive Web App (PWA)'
       });
 
