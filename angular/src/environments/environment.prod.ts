36345eb9b23322b366437654098360db6b9adc64
ganatan <dannyganatan@gmail.com>
2022-11-26 14:55:34 +0100

    Change application name on env

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   application:
   {
-    name: 'angular-starter',
+    name: 'angular-bootstrap',
     version: 'Angular 15.0.1',
     bootstrap: 'Bootstrap 5.2.3',
     fontawesome: 'Font Awesome 6.2.1',
