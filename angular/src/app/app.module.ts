790cd0e5d76f08846aa2ba1540865b4200724a16
vikranth reddy <ch.vikranth521@gmail.com>
2023-07-04 16:21:36 +0530

    test

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './modules/general/home/home.component';
 import { NotFoundComponent } from './modules/general/not-found/not-found.component';
 import { AppRoutingModule } from './app-routing.module';
-
+//
 @NgModule({
   declarations: [
     AppComponent,
