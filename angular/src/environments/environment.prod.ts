63d4f968ff786ddc585e3f5944189ca3b1cdefb1
ganatan <dannyganatan@gmail.com>
2023-08-22 15:32:43 +0200

    Update Angular 16.2.1 & react dependencies

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {
   application:
   {
     name: 'angular-bootstrap',
-    version: 'Angular 16.1.7',
+    version: 'Angular 16.2.1',
     bootstrap: 'Bootstrap 5.3.1',
-    fontawesome: 'Font Awesome 6.4.0',
+    fontawesome: 'Font Awesome 6.4.2',
   }
 };
