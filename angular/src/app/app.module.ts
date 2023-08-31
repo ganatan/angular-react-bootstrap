e22fbea59cc9d86996a4261accae2fd9642a85c7
ganatan <dannyganatan@gmail.com>
2023-08-31 07:30:15 +0200

    Update Angular 16.2.3 & Angular CLI 16.2.1

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './modules/general/home/home.component';
 import { NotFoundComponent } from './modules/general/not-found/not-found.component';
+
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
