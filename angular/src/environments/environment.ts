9a609710314c7e830e5a72876faec86229793d10
ganatan <dannyganatan@gmail.com>
2023-09-29 08:43:30 +0200

    Update Angular 16.2.7 & Angular CLI 16.2.4 & Bootstrap 5.3.2

--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -3,8 +3,8 @@ export const environment = {
   application:
   {
     name: 'angular-bootstrap',
-    version: 'Angular 16.2.4',
-    bootstrap: 'Bootstrap 5.3.1',
+    version: 'Angular 16.2.7',
+    bootstrap: 'Bootstrap 5.3.2',
     fontawesome: 'Font Awesome 6.4.2',
   }
 };
